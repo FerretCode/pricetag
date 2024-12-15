887f2c16dc599aa7d16d84215b1ad926a7173c27
Micah Cowell <micahcowell@gmail.com>
2024-12-15 23:22:27 +0000

    feature: tailwind integration

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["./ui/web/**/*.html"],
+  theme: {
+    colors: {
+      black: colors.black,
+      white: colors.white,
+      stone: colors.stone,
+      blue: colors.blue,
+      red: colors.red,
+      green: colors.green,
+      yellow: colors.yellow,
+    },
+  },
+  plugins: [],
+}
+
