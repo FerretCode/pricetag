90c201c50a4e1a0d68a804372740b54628df50eb
Micah Cowell <micahcowell@gmail.com>
2024-12-16 00:10:30 +0000

    feature: delete user confirmation

--- ui/static/scripts/users.js
+++ ui/static/scripts/users.js
@@ -1,3 +1,16 @@
-const select = document.getElementById('select-user')
-const form = document.getElementById('select-user-form')
-select.addEventListener('change', () => form.submit())
+// Automatically submit form on select value changes
+const selectUser = document.getElementById('select-user')
+const selectUserForm = document.getElementById('select-user-form')
+if (selectUser && selectUserForm) {
+    selectUser.addEventListener('change', () => selectUserForm.submit())
+}
+
+// Require delete form confirmation
+const deleteUserForm = document.getElementById('delete-user-form')
+if (deleteUserForm) {
+    deleteUserForm.addEventListener('submit', (e) => {
+        if (!confirm("Confirm user deletion. This can not be undone.")) {
+            e.preventDefault()
+        }
+    })
+}
