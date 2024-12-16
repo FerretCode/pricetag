3a4c13cbfee46e0901781a94683b4b3777c6a670
Micah Cowell <micahcowell@gmail.com>
2024-12-16 07:25:15 +0000

    styles: dashboard and users

--- ui/static/scripts/users.js
+++ ui/static/scripts/users.js
@@ -1,8 +1,17 @@
-// Automatically submit form on select value changes
+// Automatically submit select user form on select value changes
 const selectUser = document.getElementById('select-user')
 const selectUserForm = document.getElementById('select-user-form')
 if (selectUser && selectUserForm) {
     selectUser.addEventListener('change', () => selectUserForm.submit())
+}
+
+// Automatically submit permissions form on input value changes
+const permissionsForm = document.getElementById('permissions-form')
+if (permissionsForm) {
+    const permissionsInputs = permissionsForm.querySelectorAll('input')
+    permissionsInputs.forEach(input => {
+        input.addEventListener('change', () => permissionsForm.submit())
+    })
 }
 
 // Require delete form confirmation
